9256ea7c324c031d2685277da55ea85f7e8f9e4d
gecodes <graham.everitt@gmail.com>
2022-01-11 04:39:58 -0500

    fixed monitoring

--- habs_grudge_bot.py
+++ habs_grudge_bot.py
@@ -94,6 +94,7 @@ for comment in reddit.subreddit("habs").stream.comments():
             # Reply with tailored insult.
             comment.reply(insult)
             # Monitoring
+            print(comment)
             print(insult)
             # Record comment ID as being replied to.
             reply_tracker.append(comment.id)
